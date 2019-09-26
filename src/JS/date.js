2ad6e70e13465238da2279c197de85583379b5a7
T4tild3 <49338390+T4tild3@users.noreply.github.com>
2019-09-26 18:30:49 +0200

    Add files via upload

---
+++ src/JS/date.js
@@ -0,0 +1,47 @@
+var today = new Date();
+
+var gods = ['',
+            'Praios',
+            'Rondra',
+            'Efferd',
+            'Travia',
+            'Boron',
+            'Hesinde',
+            'Firun',
+            'Tsa',
+            'Phex',
+            'Peraine',
+            'Ingerimm',
+            'Rahja',
+            'Senza Nome'];
+
+function convertDate (date) {
+    var days = Math.ceil((date - new Date(date.getFullYear(),0,1)) / 86400000),
+        month;
+
+    var i = 1;
+    while (i < gods.length) {
+        if (i === Math.ceil(days/30)) { 
+            month = gods [i];
+        }
+        else if (days > 360){
+            month = gods [13];
+        }
+        i++;
+    }
+    return days + " di " + month + " in Aventuria";
+}
+
+document.write("Oggi è il giorno " + convertDate (today));
+
+function convertButton () {
+    var jsDate = $('datepicker').datepicker('getDate');
+    if (jsDate !== null) { 
+        jsDate instanceof Date;
+   
+        document.write("Il giorno selezionato è il  " + convertDate (jsDate.getDate()));
+    }
+}
+
+
+
