f3f4a406c5ec5a20bb9b8e2573af57378217b4b4
T4tild3 <cinghio92@gmail.com>
2019-10-04 12:02:43 +0200

    I have yet to implement the days of the week

--- src/JS/date.js
+++ src/JS/date.js
@@ -1,6 +1,6 @@
 var today = new Date();
 
-var gods = ['',
+var godSeason = ['',
             'Praios',
             'Rondra',
             'Efferd',
@@ -15,25 +15,70 @@ var gods = ['',
             'Rahja',
             'Senza Nome'];
 
-function convertDate (date) {
+var godMonth = ['',
+            'Firun',
+            'Tsa',
+            'Phex',
+            'Peraine',
+            'Ingerimm',
+            'Rahja',
+            'Praios',
+            'Rondra',
+            'Efferd',
+            'Travia',
+            'Boron',
+            'Hesind',
+            'Senza Nome'];
+
+function monthConvertDate (date) {
     var days = Math.ceil((date - new Date(date.getFullYear(),0,1)) / 86400000),
         month;
 
     var i = 1;
-    while (i < gods.length) {
+    while (i < godMonth.length) {
         if (i === Math.ceil(days/30)) { 
-            month = gods [i];
+            month = godMonth [i];
         }
         else if (days > 360){
-            month = gods [13];
+            month = godMonth [13];
         }
         i++;
     }
     return days + " di " + month + " in Aventuria";
 }
 
-document.write("Oggi è il giorno " + convertDate (today));
+document.write (Math.ceil((date - new Date(date.getFullYear(),0,1))));
 
-function convertButton () {
-    document.write('Il giorno selezionato è il ' + convertDate($('#datepicker').datepicker('getDate')));
+function seasonConvertDate (date) {
+    var days = Math.ceil((date - new Date(date.getFullYear(),0,1)) / 86400000),
+        month;
+
+    var i = 1;
+    while (i < godSeason.length) {
+        if (i === Math.ceil(days/30)) { 
+            month = godSeason [i];
+        }
+        else if (days > 360){
+            month = godSeason [13];
+        }
+        i++;
+    }
+    return days + " di " + month + " in Aventuria";
 }
+
+function monthConvertButton () {
+    document.write('Il giorno selezionato è il ' + monthConvertDate($('#datepicker').datepicker('getDate')));
+}
+
+function seasonConvertButton () {
+    document.write('Il giorno selezionato è il ' + seasonConvertDate($('#datepicker').datepicker('getDate')));
+}
+
+function monthToDay () {
+    return ("Oggi è il giorno " + monthConvertDate (today) + " (ordine mesi)");
+}
+
+function seasonToDay () {
+    return ("Oggi è il giorno " + seasonConvertDate (today) + " (ordine stagioni)");
+}
+
